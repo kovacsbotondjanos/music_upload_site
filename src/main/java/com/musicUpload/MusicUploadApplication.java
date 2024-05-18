4ca3fc3dbd03bbd3d887a3667220a4fb5344fbbe
kovacsbotondjanos <128900156+kovacsbotondjanos@users.noreply.github.com>
2024-05-18 18:11:10 +0200

    initial commit

---
+++ src/main/java/com/musicUpload/MusicUploadApplication.java
@@ -0,0 +1,13 @@
+package com.musicUpload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicUploadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicUploadApplication.class, args);
+	}
+
+}

b75a7806ff910795695a932acd425e7ce06d7a47
kovacsbotondjanos <128900156+kovacsbotondjanos@users.noreply.github.com>
2024-06-03 19:01:15 +0200

    work in progress

--- src/main/java/com/musicUpload/cronJobs/EntityManager.java
+++ src/main/java/com/musicUpload/cronJobs/EntityManager.java
@@ -5,44 +5,70 @@ import lombok.Data;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
+import java.util.Map;
 import java.util.Optional;
+import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 
 @Service
 public class EntityManager<T extends CustomEntityInterface> {
-    private final ConcurrentMap<Long, EntityWrapper<T>> entityMap = new ConcurrentHashMap<>();
+    private final LoadBalancerWrapper<T> loadBalancerWrapper = new LoadBalancerWrapper<>();
     private final int SCHEDULE = 15 * 1000 * 60;
     private final int REMOVE_SCHEDULE = 30 * 1000 * 60;
 
     public void addEntity(T entity) {
-        entityMap.put(entity.getId(), new EntityWrapper<>(entity));
+        loadBalancerWrapper.put(entity);
     }
 
     public void removeEntity(Long id) {
-        entityMap.remove(id);
+        loadBalancerWrapper.remove(id);
     }
 
     public Optional<T> getEntity(Long id) {
-        return entityMap.containsKey(id) ? Optional.of(entityMap.get(id).getEntity())
+        return loadBalancerWrapper.containsKey(id) ? Optional.of(loadBalancerWrapper.get(id).getEntity())
                 : Optional.empty();
     }
 
     @Scheduled(fixedRate = SCHEDULE)
     public void unCache() {
         long currentTime = System.currentTimeMillis();
-        //TODO: evaluate if i need to save the entity here, or elsewhere, but first make sure this actually works
-        entityMap.entrySet().removeIf(e -> currentTime - e.getValue().getTimeStamp() > REMOVE_SCHEDULE);
+        loadBalancerWrapper.entrySet().removeIf(e -> currentTime - e.getValue().getTimeStamp() > REMOVE_SCHEDULE);
     }
-}
 
-@Data
-class EntityWrapper<T> {
-    private T entity;
-    private long timeStamp;
+    @Data
+    static class LoadBalancerWrapper<T extends CustomEntityInterface> {
+        private final ConcurrentMap<Long, EntityWrapper<T>> entityMap = new ConcurrentHashMap<>();
 
-    public EntityWrapper(T entity) {
-        this.entity = entity;
-        timeStamp = System.currentTimeMillis();
+        public boolean containsKey(Long id) {
+            return entityMap.containsKey(id);
+        }
+
+        public EntityWrapper<T> get(Long id) {
+            return entityMap.get(id);
+        }
+
+        public void remove(Long id) {
+            entityMap.remove(id);
+        }
+
+        public Set<Map.Entry<Long, EntityWrapper<T>>> entrySet() {
+            return entityMap.entrySet();
+        }
+
+        public void put(T entity) {
+            entityMap.put(entity.getId(), new EntityWrapper<>(entity));
+        }
+
+        @Data
+        static class EntityWrapper<T> {
+            private T entity;
+            private long timeStamp;
+
+            public EntityWrapper(T entity) {
+                this.entity = entity;
+                timeStamp = System.currentTimeMillis();
+            }
+        }
     }
 }
--- src/main/java/com/musicUpload/cronJobs/SongListenCountJob.java
+++ src/main/java/com/musicUpload/cronJobs/SongListenCountJob.java
@@ -6,36 +6,42 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.concurrent.ConcurrentMap;
 
 @Service
 public class SongListenCountJob {
-    private ConcurrentMap<Long, Long> userSongMap;
+    private ConcurrentMap<Long, Long> songListensLast5Mins;
     private final SongRepository songRepository;
     private final int SCHEDULE = 5 * 1000 * 60;
 
     @Autowired
     public SongListenCountJob(SongRepository songRepository) {
         this.songRepository = songRepository;
-        userSongMap = new ConcurrentHashMap<>();
+        songListensLast5Mins = new ConcurrentHashMap<>();
     }
 
     public void addListenToSong(Long songId) {
-        userSongMap.merge(songId, 1L, Long::sum);
+        songListensLast5Mins.merge(songId, 1L, Long::sum);
     }
 
     @Scheduled(fixedRate = SCHEDULE)
     public void saveReport(){
-        var copyMap = userSongMap;
-        copyMap.forEach((s, l) -> {
-            Optional<Song> songOpt = songRepository.findById(s);
-            songOpt.ifPresent(song -> {
-                song.addListen(l);
-                songRepository.save(song);
-            });
-        });
-        userSongMap = new ConcurrentHashMap<>();
+        var copyMap = songListensLast5Mins;
+        songListensLast5Mins = new ConcurrentHashMap<>();
+        ConcurrentLinkedQueue<Song> songsToSave = new ConcurrentLinkedQueue<>();
+        copyMap.entrySet().stream()
+                .parallel()
+                .forEach(e -> {
+                    Optional<Song> songOpt = songRepository.findById(e.getKey());
+                    songOpt.ifPresent(song -> {
+                        song.addListen(e.getValue());
+                        songsToSave.add(song);
+                    });
+                });
+        songRepository.saveAll(new ArrayList<>(songsToSave));
     }
 }

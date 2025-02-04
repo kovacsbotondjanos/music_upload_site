585b88495e5b9140e55b51daa4e6daf39323a556
kovacsbotondjanos <128900156+kovacsbotondjanos@users.noreply.github.com>
2025-02-04 23:02:24 +0100

    adding frontend and new kubernetes config

---
+++ deployment-up.ps1
@@ -0,0 +1,20 @@
+#TODO: docker login when we are not already logged in on a remote server
+
+# docker build -t musicupload-spring-app ./backend
+
+# docker tag musicupload-spring-app:latest kovacsbotondjanos/musicupload-spring-app:latest
+
+# docker push kovacsbotondjanos/musicupload-spring-app:latest
+
+# docker build -t musicupload-react-app ./frontend
+
+# docker tag musicupload-react-app:latest kovacsbotondjanos/musicupload-react-app:latest
+
+# docker push kovacsbotondjanos/musicupload-react-app:latest
+
+# minikube start
+
+kubectl apply -f kubernetes-deplyment/music-upload-mysql-db-deployment.yaml
+kubectl apply -f kubernetes-deplyment/music-upload-minio-deployment.yaml
+kubectl apply -f kubernetes-deplyment/music-upload-backend-deployment.yaml
+kubectl apply -f kubernetes-deplyment/music-upload-frontend-deployment.yaml

23e2e59df398820083e559600f4562ffcc96173d
kovacsbotondjanos <128900156+kovacsbotondjanos@users.noreply.github.com>
2025-02-10 22:09:51 +0100

    refining algorithms

--- backend/src/main/java/com/musicUpload/cronJobs/SongListenCountUpdateScheduler.java
+++ backend/src/main/java/com/musicUpload/cronJobs/SongListenCountUpdateScheduler.java
@@ -1,12 +1,10 @@
 package com.musicUpload.cronJobs;
 
 import com.musicUpload.dataHandler.details.UserDetailsImpl;
-import com.musicUpload.dataHandler.models.implementations.Song;
 import com.musicUpload.dataHandler.models.implementations.UserSong;
 import com.musicUpload.dataHandler.repositories.SongRepository;
 import com.musicUpload.dataHandler.repositories.UserSongRepository;
 import com.musicUpload.util.Pair;
-import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -17,6 +15,8 @@ import java.time.ZoneId;
 import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 @Service
 @Slf4j
@@ -25,8 +25,6 @@ public class SongListenCountUpdateScheduler {
     private final UserSongRepository userSongRepository;
     private final int SCHEDULE = 2 * 1000 * 60;
     private final Map<Pair<Long, Long>, Long> songListensBuffer;
-    @Getter
-    private final Map<Pair<Long, Long>, Long> copyMap;
 
     @Autowired
     public SongListenCountUpdateScheduler(SongRepository songRepository,
@@ -34,7 +32,6 @@ public class SongListenCountUpdateScheduler {
         this.songRepository = songRepository;
         this.userSongRepository = userSongRepository;
         this.songListensBuffer = new ConcurrentHashMap<>();
-        this.copyMap = new ConcurrentHashMap<>();
     }
 
     public void addListenToSong(Long songId, UserDetailsImpl userDetails) {
@@ -47,28 +44,37 @@ public class SongListenCountUpdateScheduler {
 
     @Scheduled(fixedRate = SCHEDULE)
     public void saveReport() {
-        copyMap.putAll(songListensBuffer);
-        songListensBuffer.clear();
-        log.info("{} new listens are being saved into the database", copyMap.size());
-        new Thread(() -> {
-            Map<Long, Long> songsToSave = new ConcurrentHashMap<>();
-            Queue<UserSong> userListensToSave = new ConcurrentLinkedQueue<>();
-            //this is not optimized, i'll have to look into this in the future, but this is the best i came up for now
-            synchronized (copyMap) {
-                log.info("lock for songs starts");
-                copyMap.entrySet().stream()
+        Map<Pair<Long, Long>, Long> copyMap;
+        synchronized (songListensBuffer) {
+            copyMap = new ConcurrentHashMap<>(songListensBuffer);
+            songListensBuffer.clear();
+        }
+        try (ExecutorService executor = Executors.newSingleThreadExecutor()) {
+            log.info("{} new listens are being saved into the database", copyMap.size());
+            executor.submit(() -> {
+                Map<Long, Long> songsToSave = new ConcurrentHashMap<>();
+                Queue<UserSong> userListensToSave = new ConcurrentLinkedQueue<>();
+                //this is not optimized, i'll have to look into this in the future, but this is the best i came up for now
+                synchronized (copyMap) {
+                    log.info("lock for songs starts");
+                    copyMap.entrySet()
+                        .stream()
                         .parallel()
                         .forEach(e -> {
                             if (songsToSave.containsKey(e.getKey().getFirst())) {
-                                songsToSave.put(e.getKey().getFirst(), songsToSave.get(e.getKey().getFirst()) + e.getValue());
+                                songsToSave.put(
+                                    e.getKey().getFirst(),
+                                    songsToSave.get(e.getKey().getFirst()) + e.getValue()
+                                );
                             } else {
                                 songsToSave.put(e.getKey().getFirst(), e.getValue());
                             }
 
-                            LocalDate date = LocalDate.now();
-                            Date firstDate = Date.from(date.withDayOfMonth(1).atStartOfDay(ZoneId.systemDefault()).toInstant());
-                            Date lastDate = Date.from(date.withDayOfMonth(date.lengthOfMonth()).atStartOfDay(ZoneId.systemDefault()).toInstant());
-                            var userListenOpt = userSongRepository
+                            //monthly reports of songs
+                            Date firstDate = getFirstDayOfTheMonth();
+                            Date lastDate = getLastDayOfTheMonth();
+                            //TODO: check if we could fetch by a list of ids here
+                            Optional<UserSong> userListenOpt = userSongRepository
                                 .findBySongIdAndUserIdAndCreatedAtBetween(
                                     e.getKey().getFirst(),
                                     e.getKey().getSecond(),
@@ -77,37 +83,60 @@ public class SongListenCountUpdateScheduler {
                                 );
 
                             userListenOpt.ifPresentOrElse(
-                                    u -> {
-                                        u.setListenCount(u.getListenCount() + e.getValue());
-                                        userListensToSave.add(u);
-                                    },
-                                    () -> {
-                                        if (e.getKey().getSecond() != null) {
-                                            userListensToSave.add(
-                                                    new UserSong(
-                                                        e.getKey().getFirst(),
-                                                        e.getKey().getSecond(),
-                                                        e.getValue()
-                                                    )
-                                            );
-                                        }
+                                u -> {
+                                    u.setListenCount(u.getListenCount() + e.getValue());
+                                    userListensToSave.add(u);
+                                },
+                                () -> {
+                                    if (e.getKey().getSecond() != null) {
+                                        userListensToSave.add(
+                                            new UserSong(
+                                                e.getKey().getFirst(),
+                                                e.getKey().getSecond(),
+                                                e.getValue()
+                                            )
+                                        );
                                     }
+                                }
                             );
                         });
-                List<Song> songList = songsToSave.entrySet().stream().map(entry -> {
-                    var s = songRepository.findById(entry.getKey());
-                    if (s.isPresent()) {
-                        Song song = s.get();
-                        song.addListen(entry.getValue());
-                        return song;
-                    }
-                    return null;
-                }).filter(Objects::nonNull).toList();
-                songRepository.saveAll(songList);
-                userSongRepository.saveAll(userListensToSave);
-                log.info("lock for songs ends");
-            }
-            copyMap.clear();
-        }).start();
+
+                    songRepository.saveAll(
+                        songRepository
+                            .findAllById(songsToSave.keySet())
+                            .stream()
+                            .parallel()
+                            .peek(song -> song.addListen(songsToSave.get(song.getId())))
+                            .toList()
+                    );
+
+                    userSongRepository.saveAll(userListensToSave);
+
+                    log.info("lock for songs ends");
+                }
+                copyMap.clear();
+            });
+        }
+    }
+
+    private Date getFirstDayOfTheMonth() {
+        LocalDate date = LocalDate.now();
+        return Date.from(
+            date
+                .withDayOfMonth(1)
+                .atStartOfDay(
+                        ZoneId.systemDefault()
+                ).toInstant()
+        );
+    }
+
+    private Date getLastDayOfTheMonth() {
+        LocalDate date = LocalDate.now();
+        return Date.from(
+            date
+                .withDayOfMonth(date.lengthOfMonth())
+                .atStartOfDay(ZoneId.systemDefault())
+                .toInstant()
+        );
     }
 }
